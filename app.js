3b2d23fdfafc862e705f6d3e332f9d69f68bb23a
Protonos <96314060+Protonosgit@users.noreply.github.com>
2023-06-06 23:54:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const NodeMediaServer = require("node-media-server");
 
 const config = {
   rtmp: {
-    port: 443,
+    port: PORT,
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
     ping_timeout: 60,
   },
   http: {
-    port: 8000,
+    port: 80,
     allow_origin: "*",
   },
 };
